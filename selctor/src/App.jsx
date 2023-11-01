1ec27562664cdb10fe3c579254db4cf7825dceb3
atieh-mir <atiehmir17@gmail.com>
2023-11-01 18:44:13 +0330

    Color-selector

--- selctor/src/App.jsx
+++ selctor/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
+import { clsx } from 'clsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [color, setColor] = useState(false)
+
+ const handleOnChange =(e) =>{
+   
+      console.log(e.target.value);
+     
+     setColor(e.target.value);
+  }
 
   return (
     <>
+    <label for="color-box">  Choose a color:</label>
+     <select onClick={handleOnChange} name="color" id="color-box" >
+     <option value="">--Please choose an option--</option>
+      <option value="red-box">red</option>
+      <option value="green-box">green</option>
+      <option  value="blue-box">blue</option>
+      <option  value="yellow-box">yellow</option>
+      <option  value="black-box">black</option>
       
+        
+     </select>
+     <div className={color}
+     ></div>
+    
     </>
   )
 }
